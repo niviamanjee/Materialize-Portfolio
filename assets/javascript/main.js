a93a45389f771c5313dbcdc7a725017b6c7cfd84
Nivi Amanjee <nivedita.amanjee@gmail.com>
2019-11-17 22:58:02 -0800

    project cards added and further styling done

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,24 +1,42 @@
-$(document).ready(function () {
-    $(window).on('load scroll', function () {
-        var scrolled = $(this).scrollTop();
-        $('.navbar').css({
-            // 'transform': 'translate3d(0, ' + -(scrolled * 0.2) + 'px, 0)', // parallax (20% scroll rate)
-            'opacity': 1 - scrolled / 400 // fade out at 400px from top
-        });
-        $('#main-video').css('transform', 'translate3d(0, ' + -(scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
-    });
+// $(document).ready(function () {
+//     $(window).on('load scroll', function () {
+//         var scrolled = $(this).scrollTop();
+//         $('.navbar').css({
+//             // 'transform': 'translate3d(0, ' + -(scrolled * 0.2) + 'px, 0)', // parallax (20% scroll rate)
+//             'opacity': 1 - scrolled / 400 // fade out at 400px from top
+//         });
+//         $('#main-video').css('transform', 'translate3d(0, ' + -(scrolled * 0.25) + 'px, 0)'); // parallax (25% scroll rate)
+//     });
 
-    // video controls
-    // $('#state').on('click', function () {
-    //     var video = $('#hero-vid').get(0);
-    //     var icons = $('#state > span');
-    //     $('#overlay').toggleClass('fade');
-    //     if (video.paused) {
-    //         video.play();
-    //         icons.removeClass('fa-play').addClass('fa-pause');
-    //     } else {
-    //         video.pause();
-    //         icons.removeClass('fa-pause').addClass('fa-play');
-    //     }
-    // });
+// video controls
+// $('#state').on('click', function () {
+//     var video = $('#hero-vid').get(0);
+//     var icons = $('#state > span');
+//     $('#overlay').toggleClass('fade');
+//     if (video.paused) {
+//         video.play();
+//         icons.removeClass('fa-play').addClass('fa-pause');
+//     } else {
+//         video.pause();
+//         icons.removeClass('fa-pause').addClass('fa-play');
+//     }
+// });
+// });
+
+//carousel of projects functions 
+$('.carousel.carousel-multi-item.v-2 .carousel-item').each(function () {
+    var next = $(this).next();
+    if (!next.length) {
+        next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+
+    for (var i = 0; i < 4; i++) {
+        next = next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+    }
 });
+
